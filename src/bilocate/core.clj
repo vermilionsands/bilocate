3bbd97c3eb6eca424e30f1212c20aa54b0789476
vermilionsands <sands.vermilion@gmail.com>
2016-01-22 23:49:43 +0100

    Better args parsing for require-remote-ns

--- src/bilocate/core.clj
+++ src/bilocate/core.clj
@@ -26,19 +26,20 @@
     [(first x) (apply hash-map (rest x))]
     [x {}]))
 
-(defn require-remote-ns [& args]
-  (let [[sym {as :as referred :refer}] (parse-namespace-arg (first args))
-        reload (= (second args) :reload)]
-    (when (remote-eval `(ns-name (find-ns '~sym)))
-      (do
-        (create-ns sym)
-        (when as (alias as sym))
-        (let [remote-vars (remote-eval `(map first (ns-publics '~sym)))]
-          (doseq [name remote-vars
-                  :let [var-sym (symbol (str sym) (str name))]
-                  :when (or reload (not (find-var var-sym)))]
-              (intern sym name (partial remote-fn-call var-sym))
-              (when (or (= referred :all)
-                        (some (partial = name) referred))
-                (intern *ns* name var-sym))))))))
+(defn- require-single-namespace [ns-sym as referred overwrite]
+  (create-ns ns-sym)
+  (when as
+    (alias as ns-sym))
+  (doseq [var-sym (remote-eval `(map first (ns-publics '~ns-sym)))
+          :let [ns+var (symbol (str ns-sym) (str var-sym))]
+          :when (or overwrite (not (find-var ns+var)))]
+    (intern ns-sym var-sym (partial remote-fn-call ns+var))
+    (when (or (= referred :all) (some (partial = var-sym) referred))
+      (intern *ns* var-sym ns+var))))
 
+(defn require-remote-ns [& args]
+  (let [overwrite (some (partial = :overwrite) args)]
+    (doseq [[ns-sym {as :as referred :refer}] (map parse-namespace-arg (remove (partial = :overwrite) args))]
+      (when-not (remote-eval `(when-let [~'x (find-ns '~ns-sym)] (ns-name ~'x)))
+        (throw (IllegalArgumentException. (str "Remote namespace " ns-sym " not found."))))
+      (require-single-namespace ns-sym as referred overwrite))))
