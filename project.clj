a90af9bc6f0223375ec7cc9f854891ba6708e58e
vermilionsands <sands.vermilion@gmail.com>
2016-01-22 23:52:09 +0100

    Update

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject bilocate "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject bilocate "0.1.0"
+  :description "Utility for interaction with remote nREPLs"
+  :url "https://github.com/vermilionsands/bilocate"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
