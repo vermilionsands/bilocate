b88e6b6556fc511852e2723c558c278d478c0a6d
vermilionsands <sands.vermilion@gmail.com>
2016-01-24 19:56:12 +0100

    remote-eval tests

--- test/bilocate/core_test.clj
+++ test/bilocate/core_test.clj
@@ -1,3 +1,31 @@
 (ns bilocate.core-test
   (:require [clojure.test :refer :all]
-            [bilocate.core :refer :all]))
+            [bilocate.core :refer :all]
+            [clojure.tools.nrepl.server :as server]))
+
+(def ^:dynamic *server* nil)
+
+(defn with-remote-repl [f]
+  (with-open [server (server/start-server)]
+    (binding [*server* server
+              *nrepl-spec* {:port (:port server)}]
+      (f))))
+
+(use-fixtures :each with-remote-repl)
+
+(deftest remote-eval-test
+  (testing "Testing remote eval"
+    (is (= 3 (remote-eval '(+ 1 2))))
+    (is (= 3 (remote-eval "(+ 1 2)")))))
+
+(deftest remote-eval-out-test
+  (testing "Testing remote eval output"
+    (is (= "Test" (.trim (with-out-str (remote-eval '(println "Test"))))))
+    (is (empty? (with-out-str (remote-eval '(+ 1 2)))))))
+
+(deftest remote-eval-error-test
+  (testing "Testing remote eval exceptions"
+    (is (thrown? RuntimeException (remote-eval '(/ 1 0)))))
+  (testing "Testing remote eval exception message"
+    (let [s (try (remote-eval '(/ 1 0)) (catch Exception e (.getMessage e)))]
+      (is (= "Remote execution failed with class java.lang.ArithmeticException" s)))))
