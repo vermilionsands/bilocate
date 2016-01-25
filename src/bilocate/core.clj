a368e60eeab85a5f344eba4d6849f605c1d45b3a
vermilionsands <sands.vermilion@gmail.com>
2016-01-25 20:58:27 +0100

    refactoring and tests

--- src/bilocate/core.clj
+++ src/bilocate/core.clj
@@ -17,21 +17,29 @@
    clojure.tools.nrepl/connect (:port, :host (defaults to localhost),
    :transport-fn) and additionaly :timeout (defaults to 10000).
 
+   Supported opts:
+   :skip-return - will return nil, instead of eval result,
+                  useful for evals that return objects for which no
+                  reader tag exists
+   :skip-out    - won't print to *out*
+
    Example:
    (binding [*nrepl-spec* {:timeout 5000 :port 47319}]
      (remote-eval '(defn add [x y] (+ x y)))
      (remote-eval \"(add 2 3)\"))"
-  [form]
+  [form & opts]
   (with-open [conn (apply repl/connect (flatten (seq *nrepl-spec*)))]
-    (let [response (->
+    (let [opts-set (into #{} opts)
+          response (->
                     (repl/client conn (:timeout *nrepl-spec* 10000))
                     (repl/message {:op "eval" :code (str form)}))
           response-map (repl/combine-responses response)]
-      (when-let [out (:out response-map)]
-        (println out))
+      (when (and (not (opts-set :skip-out)) (:out response-map))
+        (println (:out response-map)))
       (when ((:status response-map) "eval-error")
         (throw (java.lang.RuntimeException. (str "Remote execution failed with " (:root-ex response-map)))))
-      (last (repl/response-values response)))))
+      (when-not (opts-set :skip-return)
+        (last (repl/response-values response))))))
 
 (defn remote-fn-call
   "Calls remote-eval passing (sym & args) as an argument."
@@ -43,44 +51,53 @@
     [(first x) (apply hash-map (rest x))]
     [x {}]))
 
-(defn- require-single-namespace [ns-sym as referred overwrite]
+(defn- require-single-namespace [ns-sym as included referred]
   (create-ns ns-sym)
-  (when as
-    (alias as ns-sym))
+  (when as (alias as ns-sym))
   (doseq [var-sym (remote-eval `(map first (ns-publics '~ns-sym)))
-          :let [ns+var (symbol (str ns-sym) (str var-sym))]
-          :when (or overwrite (not (find-var ns+var)))]
-    (intern ns-sym var-sym (partial remote-fn-call ns+var))
+          :let [ns+var-sym (symbol (str ns-sym) (str var-sym))]
+          :when (or (empty? included) (some (partial = var-sym) included))]
+    (intern ns-sym var-sym (partial remote-fn-call ns+var-sym))
     (when (or (= referred :all) (some (partial = var-sym) referred))
-      (intern *ns* var-sym ns+var))))
+      (intern (ns-name *ns*) var-sym (find-var ns+var-sym)))))
 
-(defn require-remote-ns [& args]
-  "Looks for a namespaces at a remote repl, and, if found, tries to create
+(defn require-remote-ns* [& args]
+  (doseq [[ns-sym {as :as included :include referred :refer}] (map parse-namespace-arg args)]
+    (when-not (remote-eval `(when-let [~'x (find-ns '~ns-sym)] (ns-name ~'x)))
+      (throw (IllegalArgumentException. (str "Remote namespace " ns-sym " not found."))))
+    (require-single-namespace ns-sym as included referred)))
+
+(defmacro require-remote-ns
+  "Macro that looks for a namespaces at a remote repl, and, if found, tries to create
    them locally. In each namespace it will try to create vars corresponding
-   to public vars from remote namespace. Each var would be bound to remote-fn-call
+   to symbols passed with :include option. Each var would be bound to remote-fn-call
    pointing to an appropriate remote var.
 
    Each arg can be one of the following:
 
-   Spec:
    symbol - namespace name
    vector - containing namespace name and options (in a form of pairs of keyword and value)
 
    Supported options:
    :as - takes a symbol as an argument. If present, an alias would be created in the
    current namespace.
+   :include - takes a vector of symbols to require from the namespace
    :refer - takes a vector of symbols to refer from the namespace, or the :all keyword
-   to refer all public vars.
-
-   Flag:
-   :overwrite - if the namespace already exists cause an overwrite of already defined vars.
+   to refer all included vars.
 
    Example:
-   (require-remote-ns '[remote-datasource-ns :as data] :overwrite)
+   (require-remote-ns '[remote-datasource-ns :as data :include [foo bar]])
 
    For remote nREPL connection description see remote-eval."
-  (let [overwrite (some (partial = :overwrite) args)]
-    (doseq [[ns-sym {as :as referred :refer}] (map parse-namespace-arg (remove (partial = :overwrite) args))]
-      (when-not (remote-eval `(when-let [~'x (find-ns '~ns-sym)] (ns-name ~'x)))
-        (throw (IllegalArgumentException. (str "Remote namespace " ns-sym " not found."))))
-      (require-single-namespace ns-sym as referred overwrite))))
+  [& args]
+  (doseq [[ns-sym {as :as included :include referred :refer}] (map (comp parse-namespace-arg second) args)]
+    (create-ns ns-sym)
+    (when as (alias as ns-sym))
+    (when (vector? included)
+      (doseq [var-sym included]
+        (intern ns-sym var-sym)))
+    (when (vector? referred)
+      (doseq [var-sym referred]
+        (intern *ns* var-sym))))
+  `(binding [*ns* ~*ns*]
+     (require-remote-ns* ~@args)))
--- test/bilocate/core_test.clj
+++ test/bilocate/core_test.clj
@@ -1,27 +1,51 @@
 (ns bilocate.core-test
   (:require [clojure.test :refer :all]
-            [bilocate.core :refer :all]
-            [clojure.tools.nrepl.server :as server]))
-
-(def ^:dynamic *server* nil)
+            [bilocate.core :refer :all]))
 
 (defn with-remote-repl [f]
-  (with-open [server (server/start-server)]
-    (binding [*server* server
-              *nrepl-spec* {:port (:port server)}]
-      (f))))
+  (let [proc (.exec (Runtime/getRuntime) "lein repl")
+        port (with-open [rdr (clojure.java.io/reader (.getInputStream proc))]
+               (-> rdr
+                   line-seq
+                   first
+                   (.split ":")
+                   last
+                   (Integer/parseInt)))]
+    (binding [*nrepl-spec* {:port port}]
+      (try
+        (f)
+        (finally (.destroy proc))))))
 
-(use-fixtures :each with-remote-repl)
+(defn with-remote-ns [f]
+  (remote-eval '(in-ns 'remote-bilocate-1) :skip-return)
+  (remote-eval '(do (ns remote-bilocate-1) (defn foo [x] (* 2 x))))
+  (remote-eval '(do (ns remote-bilocate-1) (defn bar [x] (* 3 x))))
+  (remote-eval '(in-ns 'remote-bilocate-2) :skip-return)
+  (remote-eval '(do (ns remote-bilocate-2) (defn zoo [x] (* 4 x))))
+  (f)
+  (remote-eval '(remove-ns 'remote-bilocate-1) :skip-return)
+  (remote-eval '(remove-ns 'remote-bilocate-2) :skip-return)
+  (remove-ns 'remove-bilocate-1)
+  (remove-ns 'remove-bilocate-2))
+
+(use-fixtures :once with-remote-repl)
+(use-fixtures :each with-remote-ns)
 
 (deftest remote-eval-test
   (testing "Testing remote eval"
     (is (= 3 (remote-eval '(+ 1 2))))
-    (is (= 3 (remote-eval "(+ 1 2)")))))
+    (is (= 3 (remote-eval "(+ 1 2)")))
+    (is (symbol? (last (remote-eval '(defn foo [x] x)))))))
 
 (deftest remote-eval-out-test
   (testing "Testing remote eval output"
     (is (= "Test" (.trim (with-out-str (remote-eval '(println "Test"))))))
     (is (empty? (with-out-str (remote-eval '(+ 1 2)))))))
+
+(deftest remote-eval-opts-test
+  (testing "Testing remote eval opts"
+    (is (nil? (remote-eval '(+ 1 2) :skip-return)))
+    (is (empty? (with-out-str (remote-eval '(println "Test") :skip-out))))))
 
 (deftest remote-eval-error-test
   (testing "Testing remote eval exceptions"
@@ -29,3 +53,35 @@
   (testing "Testing remote eval exception message"
     (let [s (try (remote-eval '(/ 1 0)) (catch Exception e (.getMessage e)))]
       (is (= "Remote execution failed with class java.lang.ArithmeticException" s)))))
+
+(deftest remote-fn-call-test
+  (testing "Testing remote-fn-call"
+    (is (= 3 (remote-fn-call '+ 1 2)))
+    (is (= "Test" (.trim (with-out-str (remote-fn-call 'println "Test")))))
+    (let [sym (last (remote-eval '(defn foo [x y] (* x y))))]
+      (is (= 6 (remote-fn-call sym 2 3))))))
+
+(deftest require-remote-ns-test
+  (testing "Testing require of remote ns"
+    (require-remote-ns '[remote-bilocate-1 :include [foo bar] :refer [bar]])
+    (is (some? (find-ns 'remote-bilocate-1)))
+    (is (= 4 (remote-bilocate-1/foo 2)))
+    (is (= 6 (bar 2)))
+    (.unbindRoot #'bar))
+  (testing "Testing overwriting already bound vars and aliases"
+    (create-ns 'remote-bilocate-2)
+    (intern 'remote-bilocate-2 'zoo (fn [x] x))
+    (is (= 3 ((find-var 'remote-bilocate-2/zoo) 3)))
+    (require-remote-ns '[remote-bilocate-2 :include [zoo] :as r])
+    (is (= 12 (r/zoo 3)))))
+
+(deftest require-multiple-ns-test
+  (testing "Testing requiring multiple ns"
+    (is (not (bound? (find-var 'bilocate.core-test/foo))))
+    (is (not (bound? (find-var 'bilocate.core-test/bar))))
+    (is (not (bound? (find-var 'bilocate.core-test/zoo))))
+    (require-remote-ns '[remote-bilocate-1 :include [foo bar] :refer [foo bar]]
+                       '[remote-bilocate-2 :include [zoo] :refer [zoo]])
+    (is (bound? (find-var 'bilocate.core-test/foo)))
+    (is (bound? (find-var 'bilocate.core-test/bar)))
+    (is (bound? (find-var 'bilocate.core-test/zoo)))))
