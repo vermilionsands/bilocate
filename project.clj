d2c1976f1d0915df044c611c5ef0d424d461a478
vermilionsands <sands.vermilion@gmail.com>
2018-07-02 20:32:42 +0200

    Bump dependencies.

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
-(defproject vermilionsands/bilocate "0.1.0"
+(defproject vermilionsands/bilocate "0.1.1"
   :description "Utility for interaction with remote nREPLs"
   :url "https://github.com/vermilionsands/bilocate"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/tools.nrepl "0.2.11"]])
-
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/tools.nrepl "0.2.12"]])
